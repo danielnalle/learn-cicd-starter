dcda95c89cfe17e450f36e5dbd76ef143478edc5
danielnalle <danielnalle31@gmail.com>
2026-07-08 20:20:56 +0800

    change unit test

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -5,7 +5,7 @@ import "testing"
 // TestAlwaysSucceeds is a simple test that will never fail.
 func TestAlwaysSucceeds(t *testing.T) {
 	expected := true
-	actual := false
+	actual := true
 
 	if actual != expected {
 		t.Errorf("Expected %v, but got %v", expected, actual)
